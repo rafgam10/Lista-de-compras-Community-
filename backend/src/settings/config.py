1517fc32f6454ac30ecb9cc37d0439a18290d5f3
Timoteo <timoteo@hdinfo-ma.com>
2026-01-16 10:21:54 -0300

    main: configuração do projeto.

--- backend/src/settings/config.py
+++ backend/src/settings/config.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 class Config:
     
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
