94fc2f49f69ee9877a5cdd8b1d3c0bbcc450ffd6
Timoteo <timoteo@hdinfo-ma.com>
2026-01-16 10:21:30 -0300

    main: Implementação da tabela produto_model.py no sqlite e a migrate dela.

---
+++ backend/src/models/produto_model.py
@@ -0,0 +1,12 @@
+from src.settings.extersions import db
+
+class Produto(db.Model):
+    
+    __tablename__ = "produtos"
+    
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    nome = db.Column(db.String, nullable=False, index=True)
+    status = db.Column(db.Boolean, nullable=False, default=True, index=True)
+    
+    def __repr(self):
+        return f"Produto: {self.id}.{self.nome} - {self.status}"
