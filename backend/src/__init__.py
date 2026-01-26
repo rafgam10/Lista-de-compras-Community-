2492683d7558b18257e5a44f46ee2b88f6218821
Timoteo <timoteo@hdinfo-ma.com>
2026-01-26 16:28:21 -0300

    feat: Implementação para estruturar configuração de tests.

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,13 +1,24 @@
 from flask import Flask
-
-from src.settings.config import Config
 from src.settings.extersions import db, migrate
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
-    app.config.from_object(Config)
-    
+
+    if testing:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    else:
+        app.config.from_object("src.settings.config.Config")
+
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # Importação dos Models (necessário pro SQLAlchemy registrar)
+    from src.models.produto_model import Produto
+    from src.models.user_model import User
+
+    # Rotas
+    from src.routes.produto_routes import produto_bp
+    app.register_blueprint(produto_bp)
 
     return app
